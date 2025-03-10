7430f6b66c1136297321e2e74cbddc72a1be8762
Sabbir Hosain <sabbirhosainbd59@gmail.com>
2025-03-10 16:47:07 +0600

    server connect success

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import database_config from "./src/config/database_config.js";
+import api_base_router from "./src/routes/api_base_router.js";
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 5000;
@@ -13,12 +15,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-// CORS policy
-app.use(cors());
 
-
-
-
+app.use(cors()); // CORS policy
+database_config(); // database connection
+app.use("/api/v1", api_base_router); // first version api base url
 
 
 // running the server GET request
